af02e4410face8e8c10b3546e4ccfd2f9a56dcbf
jr_ff <edinaldoffjunior@gmail.com>
2022-05-26 23:38:45 -0300

    Create exercicio_funcoes_camiseta.py

---
+++ exercicio_funcoes_camiseta.py
@@ -0,0 +1,4 @@
+# 8.3 - Camiseta: Escreva uma função chamada make_shirt() que aceite um tamanho e o texto de uma mensagem que deverá ser estampada na camiseta. 
+# A função deve exibir uma frase que mostre o tamanho da camiseta e a mensagem estampada. Chame a função uma vez usando argumentos posicionais para criar uma camiseta. 
+# Chame a função uma segunda vez usando argumentos nomeados.
+
